9a8e328b524f3f8fe815daa223e348739b7c80f0
Exter-dg <prkandmap23@rocketmail.com>
2021-06-26 22:26:40 +0530

    Fix Directory issues in dbconnect
    Path to autload was incorrect. Php takes path relative to index.php
    Use __DIR__ to fix this.
    Refer https://stackoverflow.com/questions/32537477/how-to-use-dir

--- sql/dbconnect.php
+++ sql/dbconnect.php
@@ -1,14 +1,18 @@
 <?php
     // Autoloader
-    if (file_exists('../vendor/autoload.php')) {
-        require_once('../vendor/autoload.php');
+    if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+        require_once(__DIR__.'/../vendor/autoload.php');
     }
+    else
+        echo"not found";
 
     // Loads .env 
     // ! It will give an error if .env does not exists
-    $dotenv = Dotenv\Dotenv::createImmutable('../');
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
     $dotenv->load();
 
+    // * Envionment variables defined in .env
+    // * Refer .env.example 
     $servername = $_ENV['SERVERNAME'];
     $username = $_ENV['DB_USERNAME'];
     $password = $_ENV['DB_PASSWORD'];
