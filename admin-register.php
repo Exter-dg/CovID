5af51f81b017538d12878f86ffadf4ba0c748caa
Exter-dg <prkandmap23@rocketmail.com>
2021-07-12 14:58:41 +0530

    Add admin register and approve files
    Now functionality for registering as a new admin and the functionality
    for existing admins to approve them is available

---
+++ admin-register.php
@@ -0,0 +1,152 @@
+<?php
+    session_start();
+    if(!isset($_SESSION["adminvalid"]) || $_SESSION["adminvalid"]!="true")
+    {
+        echo("<script type='text/javascript'>alert('Access Denied'); </script>");
+        header('Refresh: 0; URL = admin-login.html');
+    }
+?>
+
+<!-- Load dbconnect -->
+<?php
+    include_once('sql/dbconnect.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin  - Register</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Create an Admin Account!</h1>
+                            </div>
+                            <form class="user" method="post">
+                                <div class="form-group row">
+                                    <div class="col-sm-4 mb-3 mb-sm-0">
+                                        <input type="text" class="form-control form-control-user" name="fname"
+                                            placeholder="First Name">
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <input type="text" class="form-control form-control-user" name="mname"
+                                            placeholder="Middle Name">
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <input type="text" class="form-control form-control-user" name="sname"
+                                            placeholder="Last Name">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="email" class="form-control form-control-user" name="email"
+                                            placeholder="Email Address">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="text" class="form-control form-control-user" name="aadhar"
+                                            placeholder="Aadhar No.">
+                                    </div>
+
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" name="rights"
+                                            placeholder="Rights- I for Country, S-ID for state, C-SID-ID for city">
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" class="form-control form-control-user"
+                                            name="password" placeholder="Password">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" class="form-control form-control-user"
+                                            name="passwordrepeat" placeholder="Repeat Password">
+                                    </div>
+                                </div>
+                                <br>
+                                <hr>
+                                <input type="submit" name="register" value="Register Account" class="btn btn-primary btn-user btn-block">
+                            
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                            </div>
+                            <div class="text-center">
+                                <a class="small" href="admin-login.html">Already have an account? Login!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <?php
+        if(isset($_POST['register'])) {
+            $fname=$_POST['fname']; 
+            $mname=$_POST['mname'];   
+            $sname=$_POST['sname'];    
+            $email=$_POST['email'];    
+            $aadhar=$_POST['aadhar'];
+            $rights=$_POST['rights'];
+            $password=$_POST['password'];
+
+            $sql = "INSERT INTO adminapprove (fname, mname, sname, aadharno, email, rights, password) 
+                    VALUES ('$fname', '$mname', '$sname', '$aadhar', '$email', '$rights', '$password')";
+            
+            $data=$conn->query($sql);
+            if($data)
+                echo "<script>alert('Inserted Successfully')</script>";
+            else
+            { 
+                $error = "SQL: " . $sql . "\n\n" . "Error: " . $conn->error;
+                
+                // ! $error is in `` and not '' as the message itself contains " and ' inside it.
+                // ! If $error is put in those commas, an alert box would not be displayed.
+                echo "<script type='text/javascript'>alert(`$error`);</script>";
+            }  
+        }
+    ?>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
