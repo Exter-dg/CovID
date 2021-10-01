b17d73f02993ff6cd7cbb71ffa00291e904ac019
prahuljose <prahuljose@gmail.com>
2021-10-01 19:42:20 +0530

    modified admin-register button to redirect to admin-login

--- admin-register.php
+++ admin-register.php
@@ -86,7 +86,7 @@
                                 </div>
                                 <br>
                                 <hr>
-                                <input type="submit" name="register" value="Register Account" class="btn btn-primary btn-user btn-block">
+                                <input type="submit" name="register" value="Register Account" class="btn btn-primary btn-user btn-block" href="admin-login.html">
                             
                             </form>
                             <hr>
